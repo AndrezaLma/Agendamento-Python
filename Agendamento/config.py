96ff05a663da2456083b1b24dc35574dbdde8513
AndrezaLma <82479236+AndrezaLma@users.noreply.github.com>
2024-04-07 17:20:20 -0300

    Delete Agendamento directory

--- Agendamento/config.py
+++
@@ -1,6 +0,0 @@
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
-
-scope = ['https://www.googleapis.com/auth/spreadsheets']
-credentials = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope)
-client = gspread.authorize(credentials)
